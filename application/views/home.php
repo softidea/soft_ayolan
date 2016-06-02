4a3ca0c0a2b79ab995db156cf1f8940426e10549
Malinda Senanayake <Malinda Senanayake@Malinda>
2016-06-03 04:28:39 +0530

    create the login page

---
+++ application/views/home.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+    <title>Ayolan Invesments-Home</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"/>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet"/>
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet"/>
+    
+    <title>Ayolan Invesments</title>
+    
+    </head>
+    <body>
+        
+    <!--Navbar-->
+    <nav class="navbar navbar-dark bg-primary">
+        <!-- Collapse button-->
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapseEx2">
+            <i class="fa fa-bars"></i>
+        </button>
+
+        <div class="container">
+            <!--Collapse content-->
+            <div class="collapse navbar-toggleable-xs" id="collapseEx2">
+                <!--Navbar Brand-->
+                <a class="navbar-brand" href="#">Navbar</a>
+                <!--Links-->
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+                <!--Search form-->
+                <form class="form-inline">
+                    <input class="form-control" type="text" placeholder="Search">
+                </form>
+            </div>
+            <!--/.Collapse content-->
+        </div>
+    </nav>
+    <!--/.Navbar-->
+    <!--Panel-->
+        <div class="card card-block">
+            <h4 class="card-title">Panel title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+            <!--/.Panel-->
+        <?php
+        // put your code here
+        ?>
+       
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    
+    </body>
+</html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,471 +6,70 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <head>
         <meta charset="utf-8">
         <title>Ayolan|System</title>
-
-        <style type="text/css">
-
-            .cssload-thecube {
-                width: 73px;
-                height: 73px;
-                margin: 0 auto;
-                margin-top: 49px;
-                position: relative;
-                transform: rotateZ(45deg);
-                -o-transform: rotateZ(45deg);
-                -ms-transform: rotateZ(45deg);
-                -webkit-transform: rotateZ(45deg);
-                -moz-transform: rotateZ(45deg);
-            }
-            .cssload-thecube .cssload-cube {
-                position: relative;
-                transform: rotateZ(45deg);
-                -o-transform: rotateZ(45deg);
-                -ms-transform: rotateZ(45deg);
-                -webkit-transform: rotateZ(45deg);
-                -moz-transform: rotateZ(45deg);
-            }
-            .cssload-thecube .cssload-cube {
-                float: left;
-                width: 50%;
-                height: 50%;
-                position: relative;
-                transform: scale(1.1);
-                -o-transform: scale(1.1);
-                -ms-transform: scale(1.1);
-                -webkit-transform: scale(1.1);
-                -moz-transform: scale(1.1);
-            }
-            .cssload-thecube .cssload-cube:before {
-                content: "";
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background-color: rgb(43,160,199);
-                animation: cssload-fold-thecube 2.76s infinite linear both;
-                -o-animation: cssload-fold-thecube 2.76s infinite linear both;
-                -ms-animation: cssload-fold-thecube 2.76s infinite linear both;
-                -webkit-animation: cssload-fold-thecube 2.76s infinite linear both;
-                -moz-animation: cssload-fold-thecube 2.76s infinite linear both;
-                transform-origin: 100% 100%;
-                -o-transform-origin: 100% 100%;
-                -ms-transform-origin: 100% 100%;
-                -webkit-transform-origin: 100% 100%;
-                -moz-transform-origin: 100% 100%;
-            }
-            .cssload-thecube .cssload-c2 {
-                transform: scale(1.1) rotateZ(90deg);
-                -o-transform: scale(1.1) rotateZ(90deg);
-                -ms-transform: scale(1.1) rotateZ(90deg);
-                -webkit-transform: scale(1.1) rotateZ(90deg);
-                -moz-transform: scale(1.1) rotateZ(90deg);
-            }
-            .cssload-thecube .cssload-c3 {
-                transform: scale(1.1) rotateZ(180deg);
-                -o-transform: scale(1.1) rotateZ(180deg);
-                -ms-transform: scale(1.1) rotateZ(180deg);
-                -webkit-transform: scale(1.1) rotateZ(180deg);
-                -moz-transform: scale(1.1) rotateZ(180deg);
-            }
-            .cssload-thecube .cssload-c4 {
-                transform: scale(1.1) rotateZ(270deg);
-                -o-transform: scale(1.1) rotateZ(270deg);
-                -ms-transform: scale(1.1) rotateZ(270deg);
-                -webkit-transform: scale(1.1) rotateZ(270deg);
-                -moz-transform: scale(1.1) rotateZ(270deg);
-            }
-            .cssload-thecube .cssload-c2:before {
-                animation-delay: 0.35s;
-                -o-animation-delay: 0.35s;
-                -ms-animation-delay: 0.35s;
-                -webkit-animation-delay: 0.35s;
-                -moz-animation-delay: 0.35s;
-            }
-            .cssload-thecube .cssload-c3:before {
-                animation-delay: 0.69s;
-                -o-animation-delay: 0.69s;
-                -ms-animation-delay: 0.69s;
-                -webkit-animation-delay: 0.69s;
-                -moz-animation-delay: 0.69s;
-            }
-            .cssload-thecube .cssload-c4:before {
-                animation-delay: 1.04s;
-                -o-animation-delay: 1.04s;
-                -ms-animation-delay: 1.04s;
-                -webkit-animation-delay: 1.04s;
-                -moz-animation-delay: 1.04s;
-            }
-
-
-
-            @keyframes cssload-fold-thecube {
-                0%, 10% {
-                    transform: perspective(136px) rotateX(-180deg);
-                    opacity: 0;
-                }
-                25%,
-                75% {
-                    transform: perspective(136px) rotateX(0deg);
-                    opacity: 1;
-                }
-                90%,
-                100% {
-                    transform: perspective(136px) rotateY(180deg);
-                    opacity: 0;
-                }
-            }
-
-            @-o-keyframes cssload-fold-thecube {
-                0%, 10% {
-                    -o-transform: perspective(136px) rotateX(-180deg);
-                    opacity: 0;
-                }
-                25%,
-                75% {
-                    -o-transform: perspective(136px) rotateX(0deg);
-                    opacity: 1;
-                }
-                90%,
-                100% {
-                    -o-transform: perspective(136px) rotateY(180deg);
-                    opacity: 0;
-                }
-            }
-
-            @-ms-keyframes cssload-fold-thecube {
-                0%, 10% {
-                    -ms-transform: perspective(136px) rotateX(-180deg);
-                    opacity: 0;
-                }
-                25%,
-                75% {
-                    -ms-transform: perspective(136px) rotateX(0deg);
-                    opacity: 1;
-                }
-                90%,
-                100% {
-                    -ms-transform: perspective(136px) rotateY(180deg);
-                    opacity: 0;
-                }
-            }
-
-            @-webkit-keyframes cssload-fold-thecube {
-                0%, 10% {
-                    -webkit-transform: perspective(136px) rotateX(-180deg);
-                    opacity: 0;
-                }
-                25%,
-                75% {
-                    -webkit-transform: perspective(136px) rotateX(0deg);
-                    opacity: 1;
-                }
-                90%,
-                100% {
-                    -webkit-transform: perspective(136px) rotateY(180deg);
-                    opacity: 0;
-                }
-            }
-
-            @-moz-keyframes cssload-fold-thecube {
-                0%, 10% {
-                    -moz-transform: perspective(136px) rotateX(-180deg);
-                    opacity: 0;
-                }
-                25%,
-                75% {
-                    -moz-transform: perspective(136px) rotateX(0deg);
-                    opacity: 1;
-                }
-                90%,
-                100% {
-                    -moz-transform: perspective(136px) rotateY(180deg);
-                    opacity: 0;
-                }
-            }
-
-        </style>
-        <style type="text/css">
-
-            * { box-sizing:border-box; }
-
-            body {
-                font-family: Helvetica;
-                background: #eee;
-                -webkit-font-smoothing: antialiased;
-            }
-
-            hgroup { 
-                text-align:center;
-                margin-top: 4em;
-            }
-
-            h1, h3 { font-weight: 300; }
-
-            h1 { color: #636363; }
-
-            h3 { color: #4a89dc; }
-
-            form {
-                width: 380px;
-                margin: 4em auto;
-                padding: 3em 2em 2em 2em;
-                background: #fafafa;
-                border: 1px solid #ebebeb;
-                box-shadow: rgba(0,0,0,0.14902) 0px 1px 1px 0px,rgba(0,0,0,0.09804) 0px 1px 2px 0px;
-            }
-
-            .group { 
-                position: relative; 
-                margin-bottom: 45px; 
-            }
-
-            input {
-                font-size: 18px;
-                padding: 10px 10px 10px 5px;
-                -webkit-appearance: none;
-                display: block;
-                background: #fafafa;
-                color: #636363;
-                width: 100%;
-                border: none;
-                border-radius: 0;
-                border-bottom: 1px solid #757575;
-            }
-
-            input:focus { outline: none; }
-
-
-            /* Label */
-
-            label {
-                color: #999; 
-                font-size: 18px;
-                font-weight: normal;
-                position: absolute;
-                pointer-events: none;
-                left: 5px;
-                top: 10px;
-                transition: all 0.2s ease;
-            }
-
-
-            /* active */
-
-            input:focus ~ label, input.used ~ label {
-                top: -20px;
-                transform: scale(.75); left: -2px;
-                /* font-size: 14px; */
-                color: #4a89dc;
-            }
-
-
-            /* Underline */
-
-            .bar {
-                position: relative;
-                display: block;
-                width: 100%;
-            }
-
-            .bar:before, .bar:after {
-                content: '';
-                height: 2px; 
-                width: 0;
-                bottom: 1px; 
-                position: absolute;
-                background: #4a89dc; 
-                transition: all 0.2s ease;
-            }
-
-            .bar:before { left: 50%; }
-
-            .bar:after { right: 50%; }
-
-
-            /* active */
-
-            input:focus ~ .bar:before, input:focus ~ .bar:after { width: 50%; }
-
-
-            /* Highlight */
-
-            .highlight {
-                position: absolute;
-                height: 60%; 
-                width: 100px; 
-                top: 25%; 
-                left: 0;
-                pointer-events: none;
-                opacity: 0.5;
-            }
-
-
-            /* active */
-
-            input:focus ~ .highlight {
-                animation: inputHighlighter 0.3s ease;
-            }
-
-
-            /* Animations */
-
-            @keyframes inputHighlighter {
-                from { background: #4a89dc; }
-                to 	{ width: 0; background: transparent; }
-            }
-
-
-            /* Button */
-
-            .button {
-                position: relative;
-                display: inline-block;
-                padding: 12px 24px;
-                margin: .3em 0 1em 0;
-                width: 100%;
-                vertical-align: middle;
-                color: #fff;
-                font-size: 16px;
-                line-height: 20px;
-                -webkit-font-smoothing: antialiased;
-                text-align: center;
-                letter-spacing: 1px;
-                background: transparent;
-                border: 0;
-                border-bottom: 2px solid #3160B6;
-                cursor: pointer;
-                transition: all 0.15s ease;
-            }
-            .button:focus { outline: 0; }
-
-
-            /* Button modifiers */
-
-            .buttonBlue {
-                background: #4a89dc;
-                text-shadow: 1px 1px 0 rgba(39, 110, 204, .5);
-            }
-
-            .buttonBlue:hover { background: #357bd8; }
-
-
-            /* Ripples container */
-
-            .ripples {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                overflow: hidden;
-                background: transparent;
-            }
-
-
-            /* Ripples circle */
-
-            .ripplesCircle {
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                opacity: 0;
-                width: 0;
-                height: 0;
-                border-radius: 50%;
-                background: rgba(255, 255, 255, 0.25);
-            }
-
-            .ripples.is-active .ripplesCircle {
-                animation: ripples .4s ease-in;
-            }
-
-
-            /* Ripples animation */
-
-            @keyframes ripples {
-                0% { opacity: 0; }
-
-                25% { opacity: 1; }
-
-                100% {
-                    width: 200%;
-                    padding-bottom: 200%;
-                    opacity: 0;
-                }
-            }
-
-            footer { text-align: center; }
-
-            footer p {
-                color: #888;
-                font-size: 13px;
-                letter-spacing: .4px;
-            }
-
-            footer a {
-                color: #4a89dc;
-                text-decoration: none;
-                transition: all .2s ease;
-            }
-
-            footer a:hover {
-                color: #666;
-                text-decoration: underline;
-            }
-
-            footer img {
-                width: 80px;
-                transition: all .2s ease;
-            }
-
-            footer img:hover { opacity: .83; }
-
-            footer img:focus , footer a:focus { outline: none; }
-
-
-        </style>
-
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"/>
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <!-- Material Design Bootstrap -->
+        <link href="css/mdb.min.css" rel="stylesheet"/>
+        <!-- Your custom styles (optional) -->
+        <link href="css/style.css" rel="stylesheet"/>
+        <link href="../../css/bootstrap.css">
+        
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     </head>
     <body>
-
-
-        <!-- Login Form Start -->
-
-        <form style="padding-top: 0px">
-            <hgroup>
-                <h1>Ayolan Invesments</h1>
-                <h3>Managment System</h3>
-            </hgroup>
-            <div class="group">
-                <input type="text"><span class="highlight"></span><span class="bar"></span>
-                <label>Name</label>
+        
+        
+    <!--Panel-->
+    <div class="container">
+        <div class="row">
+         
+        <div class="col-sm-5">
+        <div class="card text-xs-center">
+            <div class="card-header default-color">
+                <h3 style="color: white;">Ayolan Invesments</h3>
             </div>
-            <div class="group">
-                <input type="email"><span class="highlight"></span><span class="bar"></span>
-                <label>Email</label>
-            </div>
-            <button type="button" class="button buttonBlue">
-                Login Here
-                <div class="ripples buttonRipples">
-                    <span class="ripplesCircle"></span>
+            <p>Management System</p>
+            <div class="card-block">
+                
+                <!--<h4 class="card-title">Username</h4>-->
+                <div class="md-form">
+                    <input type="text" id="form1" class="form-control">
+                    <label for="form1" class="">Username</label>
                 </div>
-            </button>
-        </form>
-        <footer>
-            <a href="#">
-                <img src="https://www.polymer-project.org/images/logos/p-logo.svg">
-            </a>
-            <p>
-                Developing by  
-                <a href="http://www.fsczone.com" target="_blank">
-                    Softidea Solution
+                <div class="md-form">
+                    <input type="password" id="form1" class="form-control">
+                    <label for="form1" class="">Password</label>
+                </div>
+                
+                <button type="button" class="btn btn-default">Login Here</button>
+            </div>
+            <div class="card-footer text-muted default-color">
+                
+                <a href="#">
+                    <img style="width: 25px;height: 25px;" src="https://www.polymer-project.org/images/logos/p-logo.svg">
                 </a>
-            </p>
-        </footer>
+                <p style="font-size: 13px;">
+                    Developing by :  
+                    <a style="color: white;text-decoration: none;font-size: 13px;"href="http://www.fsczone.com" target="_blank">
+                        Softidea Solution
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!--/.Panel-->
+        
+        
+        
+        
+        
+        
+        
+        
+
         <!-- Login Form End -->
 
         <!-- Login javascript start        -->
@@ -515,5 +114,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         <!-- Login javascript End        -->
 
+        <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/tether.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+        
     </body>
 </html>
